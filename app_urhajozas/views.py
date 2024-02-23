728686d39c7f94dd0100b371c45f49eeaed56c52
Molnár Attila <molnar.attila@szlgbp.hu>
2024-02-23 13:08:00 +0100

    urhajos adatbázis feltöltése

--- app_urhajozas/views.py
+++ app_urhajozas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Urhajos
 
 @login_required
 def feltoltes_view(request):
@@ -19,6 +20,17 @@ def urhajos_feltoltese_view(request):
 def urhajos_feltoltese_view_kuld(request):
     template = 'feltoltes_urhajos_kuld.html'
     context = {}
-    print(request.POST['tsv_szoveg'])
+    nyers = request.POST['tsv_szoveg'].strip()
+    sorok = nyers.split('\r\n')
+    for sor in sorok[1:]:
+        sortomb = sor.split('\t')
+        Urhajos.objects.create(
+            nev=sortomb[1],
+            orszag=sortomb[2],
+            nem=sortomb[3],
+            szulev=int(sortomb[4]),
+            urido=sortomb[5],
+        )
+
     return render(request, template, context)
 
